a639b3209a8ba857c827fb6432606a3bae850efe
Glatus <janipudas1@gmail.com>
2023-03-20 11:48:27 +0200

    Nyt oikeasti toimii

--- app.js
+++ app.js
@@ -2,22 +2,25 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/'), (req, res) => {
-  res.send("Test")
-}
+app.get('/', (req, res) => {
+  res.send("Test");
+});
+
 app.get('/cat', (req, res) => {
   res.send('From this endpoint you can get cats.')
 });
-//POST
-app.post('/', (req,res) => {
+// POST
+app.post('/cat', (req, res) => {
   res.send("With this endpoint you can add cats.")
 });
-//PUT
-app.put('/', (req,res) => {
-  res.send("With this endpoint you can edit cats.");
+
+// PUT
+app.put('/cat', (req, res) => {
+  res.send("With this endpoint you can edit cats.")
 });
-//DELETE
-app.delete('/', (req,res) => {
-app.send("With this endpoint you can delete cats.")
+
+// DELETE
+app.delete('/cat', (req, res) => {
+  res.send("With this endpoint you can delete cats.")
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

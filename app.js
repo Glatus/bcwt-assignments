4acf97a57ed97acb504acf2d87d8b45636a24aec
Glatus <janipudas1@gmail.com>
2023-03-20 11:35:20 +0200

    Add cat routes to app.js which respond to POST, PUT and DELETE methods. asti

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/'), (req, res) => {
+  res.send("Test")
+}
+app.get('/cat', (req, res) => {
+  res.send('From this endpoint you can get cats.')
+});
+//POST
+app.post('/', (req,res) => {
+  res.send("With this endpoint you can add cats.")
+});
+//PUT
+app.put('/', (req,res) => {
+  res.send("With this endpoint you can edit cats.");
+});
+//DELETE
+app.delete('/', (req,res) => {
+app.send("With this endpoint you can delete cats.")
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

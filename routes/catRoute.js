48be4f326d4f5e3fe4d9f0de58d6ec477b83f808
Glatus <janipudas1@gmail.com>
2023-03-20 13:28:29 +0200

    Router toimii nyt

--- routes/catRoute.js
+++ routes/catRoute.js
@@ -1,2 +1,30 @@
 'use strict';
+const express = require('express');
 // catRoute
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.send("Test");
+});
+
+router.get('/cat', (req, res) => {
+  res.send('From this endpoint you can get cats.')
+});
+//Tietty kissa
+router.get('/cat/:id', (req,res) => {
+  res.send(`You reqested a cat whose id is ${req.params.id}`);
+});
+// POST
+router.post('/cat', (req, res) => {
+  res.send("With this endpoint you can add cats.")
+});
+
+// PUT
+router.put('/cat', (req, res) => {
+  res.send("With this endpoint you can edit cats.")
+});
+
+// DELETE
+router.delete('/cat', (req, res) => {
+  res.send("With this endpoint you can delete cats.")
+});
+module.exports = router
---
+++ week2/app.js
@@ -0,0 +1,8 @@
+'use strict';
+const express = require('express');
+const { cats } = require('../models/catModel');
+const router = require('../routes/catRoute.js')
+const app = express();
+const port = 3000;
+app.use("/", router)
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

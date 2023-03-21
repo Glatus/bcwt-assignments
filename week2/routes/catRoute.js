267e4fb350de74a5fd91bf18a554aa5dbe62836a
Glatus <janipudas1@gmail.com>
2023-03-21 10:12:53 +0200

    File pitää nimen

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -3,10 +3,23 @@ const express = require('express');
 // catRoute
 const router = express.Router();
 const multer  = require('multer')
-const upload = multer({ dest: './uploads/' })
+
+// piettää kuvalle vanhan nimen
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+      callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+      const filename = file.originalname || new Date().toISOString();
+      callback(null, filename);
+  }
+});
+
+const upload = multer({ storage })
 const controller = require('../controllers/catController')
 
 router.get('/', controller.getCatList);
+
 //Tietty kissa
 router.get('/:id', controller.getCat);
 // POST

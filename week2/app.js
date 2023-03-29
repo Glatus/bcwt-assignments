c2e1cae75ff7f8b5183c4f8ce573afdf45a02699
Glatus <janipudas1@gmail.com>
2023-03-29 13:59:37 +0300

    toimi pls

--- week2/app.js
+++ week2/app.js
@@ -9,7 +9,7 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static("example-ui"))
+app.use("/",express.static("example-ui"))
 app.use("/", express.static("uploads"))
 app.use(cors())
 app.use("/cat", catRouter);

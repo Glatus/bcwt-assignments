ab1dcd507c6b29ff0f3b66eb7ee0f6919bc06769
Glatus <janipudas1@gmail.com>
2023-04-06 11:12:36 +0300

    melkein toimii

--- week2/app.js
+++ week2/app.js
@@ -1,21 +1,33 @@
 'use strict';
-require('dotenv').config()
 const express = require('express');
-var cors = require('cors')
-const { cats } = require('./models/catModel');
-const catRouter = require('./routes/catRoute.js')
-const userRouter = require('./routes/userRoute.js');
-const passport = require('./utils/passport.js');
+const cors = require('cors');
+const catRoute = require('./routes/catRoute');
+const userRoute = require('./routes/userRoute');
 const authRoute = require('./routes/authRoute');
+const passport = require('./utils/passport');
 const app = express();
 const port = 3000;
+
+// Log middleware
+app.use((req, res, next) => {
+    console.log(Date.now() + ': request: ' + req.method + ' ' + req.path);
+    next();
+});
+
+// Serve example-ui
+app.use(express.static('example-ui'));
+// Serve uploaded image files
+app.use('/uploads', express.static('uploads'));
+// Add 'Access-Control-Allow-Origin: *' header to all
+// responses using cors middleware
+app.use(cors());
+// middleware for parsing request body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/", express.static("example-ui"))
-app.use("/uploads/", express.static("uploads"))
-app.use(cors())
 app.use(passport.initialize());
+
 app.use('/auth', authRoute);
-app.use("/cat", passport.authenticate('jwt', { session: false }), catRouter);
-app.use("/user", passport.authenticate('jwt', { session: false }), userRouter);
+app.use('/cat', passport.authenticate('jwt', { session: false }), catRoute);
+app.use('/user', passport.authenticate('jwt', { session: false }), userRoute);
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- week2/controllers/catController.js
+++ week2/controllers/catController.js
@@ -1,60 +1,112 @@
 'use strict';
-// catController
 const catModel = require('../models/catModel');
-
-
-const cats = catModel.cats;
+const { validationResult } = require('express-validator');
 
 const getCatList = async (req, res) => {
     try {
-        const cats = await catModel.getAllCats();
-        console.log(cats);
+        let cats = await catModel.getAllCats();
+        // convert ISO date to date only
+        // should this be done on the front-end side??
+        cats = cats.map((cat) => {
+            cat.birthdate = cat.birthdate.toISOString().split('T')[0];
+            return cat;
+        });
         res.json(cats);
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).json({ error: 500, message: error.message });
     }
 };
 
 const getCat = async (req, res) => {
-    try {
-        const cat = await catModel.getCat(req.params.id);
+    //console.log(req.params);
+    // convert id value to number
+    const catId = Number(req.params.id);
+    // check if number is not an integer
+    if (!Number.isInteger(catId)) {
+        res.status(400).json({ error: 500, message: 'invalid id' });
+        return;
+    }
+    // TODO: wrap to try-catch
+    const [cat] = await catModel.getCatById(catId);
+    // console.log('getCat', cat);
+    if (cat) {
         res.json(cat);
+    } else {
+        // send response 404 if id not found in array
+        // res.sendStatus(404);
+        res.status(404).json({ message: 'Cat not found.' });
+    }
+};
+
+const postCat = async (req, res) => {
+    // console.log('posting a cat', req.body, req.file);
+    if (!req.file) {
+        res.status(400).json({
+            status: 400,
+            message: 'Invalid or missing image file'
+        });
+        return;
+    }
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()) {
+        res.status(400).json({
+            status: 400,
+            errors: validationErrors.array(),
+            message: 'Invalid post data'
+        });
+        return;
+    }
+    const newCat = req.body;
+    newCat.filename = req.file.filename;
+    // TODO: use req.user to add correct owner id
+    try {
+        const result = await catModel.insertCat(newCat);
+        res.status(201).json({ message: 'new cat added!' });
+    } catch (error) {
+        res.status(500).json({ error: 500, message: error.message });
+    }
+};
+
+const cat_update_put = async (req, res) => {
+    // console.log('modifying a cat', req.body);
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()) {
+        res.status(400).json({
+            status: 400,
+            errors: validationErrors.array(),
+            message: 'Invalid PUT data'
+        });
+        return;
+    }
+    const cat = req.body;
+    // TODO: before modifying a cat you should check that user is the owner of
+    // that specific cat (req.user.user_id == cat.owner). Can be done in catModel!
+    try {
+        const result = await catModel.modifyCat(cat);
+        res.status(200).json({ message: 'cat modified!' });
+    } catch (error) {
+        res.status(500).json({ error: 500, message: error.message });
+    }
+};
+
+const cat_delete = async (req, res) => {
+    // console.log('deleting a cat', req.params.id);
+    try {
+        const result = await catModel.deleteCat(req.params.id);
+        res.status(200).json({ message: 'cat deleted!' });
+    } catch (error) {
+        res.status(500).json({ error: 500, message: error.message });
+    }
+};
+
+const cat_create_post = async (req, res) => {
+    try {
+        catModel.addCat(req);
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
     }
 };
-
-
-const postCat = async (req, res) => {
-      console.log(req.body);
-}
-const cat_create_post = async (req,res) => {
-    try {
-      catModel.addCat(req)
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error')
-    }
-  };
-const cat_update_put = async (req,res) => {
-    try {
-        catModel.updateCat(req.body)
-    } catch (error) {
-        console.error(error);
-      res.status(500).send('Internal Server Error')
-    }
-}
-const cat_delete = async (req,res) => {
-    try {
-        catModel.deleteCat(req.params.id)
-        res.json({message: 'Cat deleted!'})
-    } catch (error) {
-        console.error(error);
-      res.status(500).send('Internal Server Error')
-    }
-}
 module.exports = {
     getCatList, getCat, postCat, cat_create_post, cat_update_put, cat_delete
 };
